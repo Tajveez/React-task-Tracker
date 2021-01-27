640d2ac0c84124f8880cdb86bbf78f4cc528e3b9
Tajveez <Tajveez786@gmail.com>
2021-01-27 23:40:55 +0500

    Task component use state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Bring Eggs",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Project Meeting",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,8 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Bring Eggs",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Project Meeting",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Food Shopping",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-];
-
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3>{task.text}</h3>
+        <h3 key={task.id}>{task.text}</h3>
       ))}
     </>
   );
