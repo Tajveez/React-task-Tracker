2c4220db71bfc01203cacfb500c539c38ee5a311
Tajveez <Tajveez786@gmail.com>
2021-01-30 23:04:27 +0500

    Toggle Reminder

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       id: 2,
       text: "Project Meeting",
       day: "Feb 5th at 2:30pm",
-      reminder: true,
+      reminder: false,
     },
     {
       id: 3,
@@ -28,11 +28,24 @@ function App() {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks
+          tasks={tasks}
+          onDelete={deleteTask}
+          toggleReminder={toggleReminder}
+        />
       ) : (
         "No tasks Available"
       )}
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,11 @@
 import { FaTimes } from "react-icons/fa";
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, toggleReminder }) => {
   return (
-    <div className="task">
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => toggleReminder(task.id)}
+    >
       <h3>
         {task.text}
         <FaTimes
